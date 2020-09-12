7a62589de2a6aa5c73b907b3bdfbeb3c7335aabf
Valentin <ValentinSpivak@gmail.com>
2020-09-12 22:40:20 +0300

    Router
    (Dialog, Profile)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,17 +4,24 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/nav/nav";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+
 
 const App = (props) => {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <Navbar/>
-            <div className="app-wrapper-content">
-                <Dialogs />
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <div className="app-wrapper-content">
+                    <Route path='/Dialogs' component={Dialogs}/>
+                    <Route path='/Profile' component={Profile}/>
+                    {/*<Route path='/Dialogs' component={Dialogs}/>*/}
+                    {/*<Route path='/Profile' component={Profile}/>*/}
+                    {/*<Route path='/Profile' component={Profile}/>*/}
+                </div>
             </div>
-            {/*<Profile />*/}
-        </div>
+        </BrowserRouter>
     );
 };
 
--- my-app/src/components/nav/nav.jsx
+++ my-app/src/components/nav/nav.jsx
@@ -2,25 +2,25 @@ import React from "react";
 import s from "./nav.module.css";
 
 const Navbar = () => {
-  return (
-    <nav className={s.nav}>
-      <div>
-        <a href="https://github.com/Valentin-Sp/my-first-repo" className={s.item}>Profile</a>
-      </div>
-      <div>
-        <a href="https://github.com/Valentin-Sp/my-first-repo" className={s.item}>Massages</a>
-      </div>
-      <div>
-        <a href="https://github.com/Valentin-Sp/my-first-repo" className={s.item}>News</a>
-      </div>
-      <div>
-        <a href="https://github.com/Valentin-Sp/my-first-repo" className={s.item}>Music</a>
-      </div>
-      <div>
-        <a href="https://github.com/Valentin-Sp/my-first-repo" className={s.item}>Settings</a>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className={s.nav}>
+            <div>
+                <a href="/profile" className={s.item}>Profile</a>
+            </div>
+            <div>
+                <a href="/dialogs" className={s.item}>Massages</a>
+            </div>
+            <div>
+                <a href="https://github.com/Valentin-Sp/my-first-repo" className={s.item}>News</a>
+            </div>
+            <div>
+                <a href="https://github.com/Valentin-Sp/my-first-repo" className={s.item}>Music</a>
+            </div>
+            <div>
+                <a href="https://github.com/Valentin-Sp/my-first-repo" className={s.item}>Settings</a>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
