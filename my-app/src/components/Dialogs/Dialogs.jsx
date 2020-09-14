b5e0ebab4f4478b5dbc201f1ded1924a3b1264b0
Valentin <ValentinSpivak@gmail.com>
2020-09-14 11:03:17 +0300

    Dialogs
    grid layout

--- my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,22 @@
 import React from "react";
-// import s from "./Dialogs.module.css";
+import s from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs 123 Helloy
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>Dimych</div>
+                <div className={s.dialog}>Andrey</div>
+                <div className={s.dialog}>Sveta</div>
+                <div className={s.dialog}>Sasha</div>
+                <div className={s.dialog}>Viktor</div>
+                <div className={s.dialog}>Valeria</div>
+            </div>
+            <div className={s.massages}>
+                <div className={s.massage}>Hi</div>
+                <div className={s.massage}>How is you learning&</div>
+                <div className={s.massage}>Yo bro!</div>
+            </div>
         </div>
     )
 };
