15a6aee3cb17bc03d9bd2816b2d6b04137c744e2
Valentin <ValentinSpivak@gmail.com>
2021-01-31 20:27:57 +0200

    Practice - reducer (almost redux)

--- my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -3,7 +3,7 @@ import s from "./Dialogs.module.css";
 // import {NavLink} from "react-router-dom";
 import DialogItem from "./DialogItem/DialogItems";
 import Message from "./Message/Message";
-import {sendMessageCreator, updateNewMessageBodyCreator} from "../../Redux/state";
+import {sendMessageCreator, updateNewMessageBodyCreator} from "../../redux/dialogs-reducer";
 
 const Dialogs = (props) => {
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,5 +1,5 @@
 import * as serviceWorker from './serviceWorker';
-import store from "./Redux/state";
+import store from "./redux/state";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
