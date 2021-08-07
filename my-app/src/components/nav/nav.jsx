b585b01fed7139570b735421a725d39e8d42265c
Valentyn <valentinspivak@gmail.com>
2021-08-07 12:30:00 +0300

    users add

--- my-app/src/components/nav/nav.jsx
+++ my-app/src/components/nav/nav.jsx
@@ -1,27 +1,42 @@
-import React from "react";
-import s from "./nav.module.css";
-import {NavLink} from "react-router-dom";
+import React from 'react'
+import s from './nav.module.css'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
-    return (
-        <nav className={s.nav}>
-            <div className={s.item}>
-                <NavLink to="/profile" activeClassName={s.activeLink}>Profile</NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to="/dialogs" activeClassName={s.activeLink}>Messages</NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to="/News" activeClassName={s.activeLink}>News</NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to="/Music" activeClassName={s.activeLink}>Music</NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to="/Settings" activeClassName={s.activeLink}>Settings</NavLink>
-            </div>
-        </nav>
-    );
-};
+  return (
+    <nav className={s.nav}>
+      <div className={s.item}>
+        <NavLink to="/profile" activeClassName={s.activeLink}>
+          Profile
+        </NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/dialogs" activeClassName={s.activeLink}>
+          Messages
+        </NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/users" activeClassName={s.activeLink}>
+          Users
+        </NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/News" activeClassName={s.activeLink}>
+          News
+        </NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/Music" activeClassName={s.activeLink}>
+          Music
+        </NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/Settings" activeClassName={s.activeLink}>
+          Settings
+        </NavLink>
+      </div>
+    </nav>
+  )
+}
 
-export default Navbar;
+export default Navbar
