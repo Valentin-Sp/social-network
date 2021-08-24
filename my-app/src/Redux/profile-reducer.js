ab882d13ab2912c4e67e41345c917361bd7488c6
Valentyn <valentinspivak@gmail.com>
2021-08-24 19:48:54 +0300

    67 - React JS - урок redux-thunk 2 в деталях (практика)

--- my-app/src/Redux/profile-reducer.js
+++ my-app/src/Redux/profile-reducer.js
@@ -1,3 +1,5 @@
+import {usersAPI} from "../api/api";
+
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
@@ -42,6 +44,13 @@ const profileReducer = (state = initialState, action) => {
 
 export const addPostActionCreator = () => ({type: 'ADD-POST'})
 export const setUsersProfile = (profile) => ({type: SET_USER_PROFILE, profile})
+
+export const getUsersProfile = (userId) => (dispatch) => {
+    usersAPI.getProfile(userId).then((response) => {
+        dispatch(setUsersProfile(response.data))
+    })
+}
+
 export const updateNewPostTextActionCreator = (text) =>
     ({type: 'UPDATE-NEW-POST-TEXT', newText: text})
 
