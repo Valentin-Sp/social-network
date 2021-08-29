4668d294eccadb3a5da954a38d9e9cad8c8dff9a
Valentyn <valentinspivak@gmail.com>
2021-08-29 16:26:48 +0300

    73 - React JS - http put, local state

--- my-app/src/components/Profile/ProfileContainer.jsx
+++ my-app/src/components/Profile/ProfileContainer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Profile from "./Profile";
 import {connect} from "react-redux";
-import {getUsersProfile} from "../../redux/profile-reducer";
+import {getStatus, getUsersProfile, updateStatus} from "../../redux/profile-reducer";
 import {Redirect, withRouter} from "react-router-dom";
 import {withAuthRedirect} from "../../hoc/withAuthRedirect";
 import {compose} from "redux";
@@ -16,21 +16,22 @@ class ProfileContainer extends React.Component {
             userId = 2
         }
         this.props.getUsersProfile(userId)
+        this.props.getStatus(userId)
     }
 
     render() {
         return (
-            <Profile {...this.props} profile={this.props.profile}/>
+            <Profile {...this.props} profile={this.props.profile} status={this.props.status} updateStatus={this.props.updateStatus}/>
         )
     }
 }
 
 let mapStateToProps = (state) => ({
     profile: state.profilePage.profile,
+    status: state.profilePage.status
 })
 
 export default compose(
-    connect(mapStateToProps, {getUsersProfile}),
+    connect(mapStateToProps, {getUsersProfile, getStatus, updateStatus}),
     withRouter,
-    // withAuthRedirect
 )(ProfileContainer)
--- my-app/src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ my-app/src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import s from "./ProfileInfo.module.css";
 import Preloader from "../../../common/Preloader/Preloader";
 import ProfileStatus from "./ProfileStatus"
+import {updateStatus} from "../../../redux/profile-reducer";
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -17,7 +18,7 @@ const ProfileInfo = (props) => {
             {/*</div>*/}
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large}/>
-                <ProfileStatus status={'Helloy my friends'}/>
+                <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
             </div>
         </div>
     );
