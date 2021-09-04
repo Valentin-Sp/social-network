992b8a948b9fae78a5e29f1b47c49c6a805312ac
Valentyn <valentinspivak@gmail.com>
2021-09-04 17:36:23 +0300

    79 - React JS - stopSubmit (redux-form)

--- my-app/src/components/Profile/ProfileContainer.jsx
+++ my-app/src/components/Profile/ProfileContainer.jsx
@@ -13,7 +13,7 @@ class ProfileContainer extends React.Component {
     componentDidMount() {
         let userId = this.props.match.params.userId
         if (!userId) {
-            userId = 2
+            userId = this.props.authorizedUserId
     }
         this.props.getUsersProfile(userId)
         this.props.getStatus(userId)
@@ -21,14 +21,19 @@ class ProfileContainer extends React.Component {
 
     render() {
         return (
-            <Profile {...this.props} profile={this.props.profile} status={this.props.status} updateStatus={this.props.updateStatus}/>
+            <Profile {...this.props}
+                     profile={this.props.profile}
+                     status={this.props.status}
+                     updateStatus={this.props.updateStatus}/>
         )
     }
 }
 
 let mapStateToProps = (state) => ({
     profile: state.profilePage.profile,
-    status: state.profilePage.status
+    status: state.profilePage.status,
+    authorizedUserId: state.auth.userId,
+    isAuth: state.auth.isAuth
 })
 
 export default compose(
