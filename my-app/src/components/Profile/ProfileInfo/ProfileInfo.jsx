12968e4d030dc311556e072480bfe9111d4bf2b7
Valentyn <valentinspivak@gmail.com>
2021-09-09 11:58:37 +0300

    84 - React JS - hook, useState, хуки

--- my-app/src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ my-app/src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,6 +3,7 @@ import s from "./ProfileInfo.module.css";
 import Preloader from "../../common/Preloader/Preloader";
 import ProfileStatus from "./ProfileStatus"
 import {updateStatus} from "../../../redux/profile-reducer";
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -18,7 +19,7 @@ const ProfileInfo = (props) => {
             {/*</div>*/}
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large}/>
-                <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+                <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
             </div>
         </div>
     );
